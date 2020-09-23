95955bb78cd8c3306a6be3e43515592f782adb75
joaoleocadio <joaocarvalho388@gmail.com>
2020-09-23 15:19:22 +0100

    Project created

---
+++ src/chesssystem/ChessSystem.java
@@ -0,0 +1,16 @@
+package chesssystem;
+
+/**
+ *
+ * @author Utilizador
+ */
+public class ChessSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
